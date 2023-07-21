3e883e3528c0cde343bbc2f8794c717405032cc8
Salimata Azzolini <100932041+SalimataAzzolini@users.noreply.github.com>
2023-07-21 10:56:40 +0200

    Update UserList.js

--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -7,24 +7,20 @@ const UserList = () => {
   const [listData, setListData] = useState([]);
 
   useEffect(() => {
-    //On recupère l'id des films coup de coeur dans le locl sotarge
     let moviesId = window.localStorage.movies
-      ? window.localStorage.movies.split(",") // on sépare les tableau avec split
+      ? window.localStorage.movies.split(",") 
       : [];
 
     for (let i = 0; i < moviesId.length; i++) {
-      // On fait un boucle sur tous les élements de coup de coeur
-      //Puis par la suite on les intègres dans le axios et i pour le tour de boucle dans lequel il est
       axios
         .get(
           `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`
         )
-        .then((res) => setListData((listData) => [...listData, res.data])); //Ici avec ... ou spred opérateur
-      // on casse le tableau en cours et on ajoute res.data tous les films
+        .then((res) => setListData((listData) => [...listData, res.data]));
     }
   }, []);
 
-  //  III. AFFICHAGE
+
   return (
     <div className="user-list-page">
       <Header />
